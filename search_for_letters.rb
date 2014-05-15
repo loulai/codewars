79b9913cb19cb270a2af27bd778a954143ed8c30
Louise <louiselai88@gmail.com>
2014-05-15 20:22:40 +0100

    doesn't work yetgit status Read instructions wrong. starting again

---
+++ search_for_letters.rb
@@ -0,0 +1,22 @@
+# #String search
+
+# Create a method to accept one arbitrary string as an argument, and return a string of length 26.
+
+# The objective is to detect if letters of the alphabet (upper or lower case) are present anywhere in the string, and to set each of the 26 characters corresponding to each letter to either '1' if present, '0' if not.
+
+# So if an 'a' or an 'A' appears anywhere in the argument string (any number of times), set the first character of the returned string to '1' otherwise to '0', if 'b' or 'B' the the second to '1', and so on for the rest of the alphabet.
+
+# So the returned string consists only of '1's and '0's.
+
+
+def change a 
+  zero_array = ("0"*26).chars
+  alphabet = [*"a".."z"]
+  top_alphabet = a.downcase.chars.sort.first
+  top_index = alphabet.index(top_alphabet)
+  zero_array[top_index] = "1"
+  end_result = zero_array.join
+  print end_result
+end
+
+change("cehs")
---
+++ yield_to_block.rb
@@ -0,0 +1,4 @@
+# Complete the Compute method/function so that if a block is given it will run else it returns "Do not compute".
+def compute 
+	print "hello"
+end
