ba08b84b4ed62abba0f28ae869d70273e4e24eb9
Louise <louiselai88@gmail.com>
2014-05-22 11:44:23 +0100

    success

--- say_hello.rb
+++ say_hello.rb
@@ -1,7 +1,7 @@
 # Create a method say_hello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space betweeen each, and the length of the name array in test cases will vary.
 
-Example
 
 def say_hello(name, city, state)
-
+	name_string = name.join(' ')
+	"Hello, #{name_string}! Welcome to #{city}, #{state}!"
 end
