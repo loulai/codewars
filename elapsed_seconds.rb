76227e1fbb4d5e0913d2146f39ed1a67445126e3
Louise <louiselai88@gmail.com>
2014-05-24 16:52:54 +0100

    can calculate elapsed secs

---
+++ elapsed_seconds.rb
@@ -0,0 +1,5 @@
+#Complete the function so that it returns the number of seconds that have elapsed between the start and end times given.
+
+def elapsed_seconds(start_time, end_time)
+  (end_time - start_time).round
+end
