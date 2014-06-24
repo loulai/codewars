5be30324abf39c463b9e58f1cc372297a49164b9
Louise <louiselai88@gmail.com>
2014-06-24 15:02:18 +0100

    double proving (to myself) what the inject method does, and how

--- unlimited_sum.rb
+++ unlimited_sum.rb
@@ -2,6 +2,6 @@
 # Write a method sum that accepts an unlimited number of integer arguments, and adds all of them together.
 # The method should reject any arguments that are not integers, and sum the remaining integers.
 
-print [1,2,3,4].inject {|acc, num| acc + num}
+print [1,2,3,4].inject {|acc, num| "(" + acc.to_s + num.to_s + ")"}
 
 
