672570959167382cade3efb310eeb6c666b5d464
Louise <louiselai88@gmail.com>
2014-06-24 16:12:32 +0100

    final answer, not yet refactored. Works

--- unlimited_sum.rb
+++ unlimited_sum.rb
@@ -2,14 +2,9 @@
 # Write a method sum that accepts an unlimited number of integer arguments, and adds all of them together.
 # The method should reject any arguments that are not integers, and sum the remaining integers.
 
-# def sum(*num)
-
-# 	num.count == 1 ? num.first : num.inject(:+)
-# end
-
-arr =  ["boo", "monster", 2, 4, 66, 7].select {|elem| elem.to_s =~ /\d/}
-
-puts arr.any? {|x| x.is_a?(String)}
-
+def sum(*num)
+  num = num.select {|x| x.to_s =~ /\d+$/}
+  num.count == 1 ? num.first : num.inject(&:+)
+end
 
 
