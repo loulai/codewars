64a421f9ce493ac33d68b7e10e0e76a182c048bb
Louise <louiselai88@gmail.com>
2014-06-24 15:06:50 +0100

    first solution, I think its nice and concise

--- unlimited_sum.rb
+++ unlimited_sum.rb
@@ -2,6 +2,9 @@
 # Write a method sum that accepts an unlimited number of integer arguments, and adds all of them together.
 # The method should reject any arguments that are not integers, and sum the remaining integers.
 
-print [1,2,3,4].inject {|acc, num| "(" + acc.to_s + num.to_s + ")"}
+def sum(*num)
+	num.count == 1 ? num.first : num.inject(:+)
+end
+
 
 
