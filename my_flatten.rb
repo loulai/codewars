d20dc73f0e1d9adb04f40978e6cf410b4c6e370c
Louise <louiselai88@gmail.com>
2014-07-27 21:57:46 +1000

    minor change

--- my_flatten.rb
+++ my_flatten.rb
@@ -1,4 +1,4 @@
-# Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
+\\# Write a function that flattens an Array of Array objects into a flat Array. Your function must only do one level of flattening.
 
 def flatten(array)
 
